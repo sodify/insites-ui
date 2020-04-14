edb9d7b5930d9714ed9054891ffa6b9f9394c339
Tomek Marciniak <tmarciniakm@gmail.com>
2020-04-14 19:36:15 +0200

    fix(exports): create dist

--- dist/FormControl/index.d.ts
+++ dist/FormControl/index.d.ts
@@ -1,8 +1,2 @@
-import Box from '../Box';
-interface LabelProps {
-    htmlFor: string;
-}
-declare const Label: import("styled-components").StyledComponent<"label", any, LabelProps, never>;
-declare const Error: import("styled-components").StyledComponent<"div", any, {}, never>;
-export { Label, Error };
-export default Box;
+declare const FormControl: any;
+export default FormControl;
